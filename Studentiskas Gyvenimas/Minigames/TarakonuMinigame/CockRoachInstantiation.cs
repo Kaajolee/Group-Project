ee6cbe5248591ae5ea61f84f6a3508ebfbfa045b
Karolis <latoza959@gmail.com>
2024-03-16 16:07:42 +0200

    pause menu obj

--- Studentiskas Gyvenimas/Minigames/TarakonuMinigame/CockRoachInstantiation.cs
+++ Studentiskas Gyvenimas/Minigames/TarakonuMinigame/CockRoachInstantiation.cs
@@ -7,75 +7,99 @@ public partial class CockRoachInstantiation : Node2D
 {
 	// Called when the node enters the scene tree for the first time.
 	PackedScene cockRoachScene;
-    Random rnd;
-    public int score;
-    Label scoreLabel;
-    Global global;
-    CustomSignals customSignals;
+	PackedScene pauseMenu;
+	Random rnd;
+	public int score;
+	Label scoreLabel;
+	Global global;
+	CustomSignals customSignals;
 
 	public override void _Ready()
 	{
-        cockRoachScene = ResourceLoader.Load<PackedScene>("res://Minigames/TarakonuMinigame/CockRoach.tscn");
-        scoreLabel = GetNode<Label>("CanvasLayer/Panel/Label");
-        customSignals = GetNode<CustomSignals>("/root/CustomSignals");
-        global = GetNode<Global>("/root/Global");
+		cockRoachScene = ResourceLoader.Load<PackedScene>("res://Minigames/TarakonuMinigame/CockRoach.tscn");
 
-        //customSignals.CockroachMinigameEnded += GameEnded;
+		pauseMenu = ResourceLoader.Load<PackedScene>("");
+		Node2D menuNode = (Node2D)pauseMenu.Instantiate();
+		menuNode.Visible = true;
+		AddChild(menuNode);
 
-        rnd = new Random();
+		scoreLabel = GetNode<Label>("CanvasLayer/Panel/Label");
+		customSignals = GetNode<CustomSignals>("/root/CustomSignals");
+		global = GetNode<Global>("/root/Global");
 
-    }
+		//customSignals.CockroachMinigameEnded += GameEnded;
+
+		rnd = new Random();
+
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-        UpdateLabelText();
-	}
+		UpdateLabelText();
+
+        if (Input.IsActionJustPressed("escape"))
+        {
+			PauseGame();
+        }
+    }
 	void OnTimerTimeout()
 	{
-        InstantiateCockroach();
-    }
-    void InstantiateCockroach()
-    {
-        Debug.WriteLine("--------------------------");
-        Vector2 position = PositionGenerator();
-        Node2D scene = (Node2D)cockRoachScene.Instantiate();
-        float randomFloat = rnd.NextSingle();
-        float randomScale = randomFloat <= 0.2f ? 0.2f : randomFloat;
-        Debug.WriteLine("Scale: " + randomScale);
-        scene.Scale = new Vector2(randomScale, randomScale);
-        scene.Position = position;
+		InstantiateCockroach();
+	}
+	void InstantiateCockroach()
+	{
+		Debug.WriteLine("--------------------------");
+		Vector2 position = PositionGenerator();
+		Node2D scene = (Node2D)cockRoachScene.Instantiate();
+		float randomFloat = rnd.NextSingle();
+		float randomScale = randomFloat <= 0.2f ? 0.2f : randomFloat;
+		scene.Scale = new Vector2(randomScale, randomScale);
+		scene.Position = position;
 
 
-        AddChild(scene);
+		AddChild(scene);
 
-        Debug.WriteLine($"Object instantiated: {position}, scale: {randomScale}");
-    }
-    Vector2 PositionGenerator()
-    {
-        var currentViewport = GetViewport().GetVisibleRect().Size + new Vector2(100,100);
-        Vector2 generatedPosition = new Vector2();
-        switch (rnd.Next(4))
+		Debug.WriteLine($"Object instantiated: {position}, scale: {randomScale}");
+	}
+	Vector2 PositionGenerator()
+	{
+		var currentViewport = GetViewport().GetVisibleRect().Size + new Vector2(100,100);
+		Vector2 generatedPosition = new Vector2();
+		switch (rnd.Next(4))
+		{
+			case 0:
+				generatedPosition = new Vector2(rnd.Next((int)currentViewport.X), 0);
+				break;
+			case 1:
+				generatedPosition = new Vector2(currentViewport.X, 0);
+				break;
+			case 2:
+				generatedPosition = new Vector2(rnd.Next((int)currentViewport.X), currentViewport.Y);
+				break;
+			case 3:
+				generatedPosition = new Vector2(0, rnd.Next((int)currentViewport.Y));
+				break;
+		}
+		return generatedPosition;
+
+	}
+	void UpdateLabelText()
+	{
+		scoreLabel.Text = "Score: " + global.cockroachScore.ToString();
+	}
+	void PauseGame()
+	{
+		bool state = GetTree().Paused;
+
+		if (state)
+		{
+			GetTree().Paused = false;
+		}
+        else
         {
-            case 0:
-                generatedPosition = new Vector2(rnd.Next((int)currentViewport.X), 0);
-                break;
-            case 1:
-                generatedPosition = new Vector2(currentViewport.X, 0);
-                break;
-            case 2:
-                generatedPosition = new Vector2(rnd.Next((int)currentViewport.X), currentViewport.Y);
-                break;
-            case 3:
-                generatedPosition = new Vector2(0, rnd.Next((int)currentViewport.Y));
-                break;
+            GetTree().Paused = true;
         }
-        return generatedPosition;
-
-    }
-    void UpdateLabelText()
-    {
-        scoreLabel.Text = "Score: " + global.cockroachScore.ToString();
     }
 
 }
