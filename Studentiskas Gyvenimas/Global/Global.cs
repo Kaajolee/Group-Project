27edc3d204ecaf87c50f3e290281deccdc5b2bd4
Chaosas <giezaj11@gmail.com>
2024-04-22 10:41:35 +0300

    bug fix(nera veikejo)

--- Studentiskas Gyvenimas/Global/Global.cs
+++ Studentiskas Gyvenimas/Global/Global.cs
@@ -9,13 +9,13 @@ public partial class Global : Node
 	public int cockroachScore { get; set; } = 0;
 	public int parkingScore { get; set; } = 0;
 	public int typerScore { get; set; } = 0;
-    public int bookScore { get; set; } = 0;
+	public int bookScore { get; set; } = 0;
 
-    public void CurrentScore()
+	public void CurrentScore()
 	{
-        string rez = string.Format("Cockroach: {0}\nParking: {1}\nTyper: {2}\nTotal: {3}",
-            cockroachScore, parkingScore, typerScore, Sum());
-        Console.WriteLine(rez);
+		string rez = string.Format("Cockroach: {0}\nParking: {1}\nTyper: {2}\nTotal: {3}",
+			cockroachScore, parkingScore, typerScore, Sum());
+		Console.WriteLine(rez);
 	}
 	public int Sum()
 	{
