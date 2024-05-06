3dc3b67a57e2131299cf44334315ea2a5561c99f
3m1lija <kardeliai.at@gmail.com>
2024-05-06 11:21:48 +0300

    Modified the design of buttons in the selection menu

--- Studentiskas Gyvenimas/Main/Scenes/interaction/InteractionManager.gd
+++ Studentiskas Gyvenimas/Main/Scenes/interaction/InteractionManager.gd
@@ -4,7 +4,7 @@ extends Node2D
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var label = $Label
 
-const base_text = "[E] to "
+const base_text = "[E] "
 
 var active_areas = []
 var can_interact = true
