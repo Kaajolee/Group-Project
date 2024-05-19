800c74a26b2ac1a021fe41af27ffeecd3ddb63f9
Chaosas <giezaj11@gmail.com>
2024-05-20 01:41:20 +0300

    Žaidimo pabaiga

--- Studentiskas Gyvenimas/Main/Scenes/interaction/areas/EndGame.gd
+++ Studentiskas Gyvenimas/Main/Scenes/interaction/areas/EndGame.gd
@@ -8,5 +8,6 @@ func _ready():
 	interaction_area.interact = Callable(self, "_on_interact")
 	
 func _on_interact():
-	get_tree().quit()
+	DialogueManager.show_example_dialogue_balloon(load("res://Dialogai/gameend.dialogue"), "start")
+	return
 
