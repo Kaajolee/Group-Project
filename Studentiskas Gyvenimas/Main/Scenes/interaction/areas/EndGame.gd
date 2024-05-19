e2bf5250985281f217617de5ef8b804db0640722
Chaosas <giezaj11@gmail.com>
2024-05-19 12:48:49 +0300

    panaikinti failai

--- Studentiskas Gyvenimas/Main/Scenes/interaction/areas/EndGame.gd
+++ Studentiskas Gyvenimas/Main/Scenes/interaction/areas/EndGame.gd
@@ -8,6 +8,5 @@ func _ready():
 	interaction_area.interact = Callable(self, "_on_interact")
 	
 func _on_interact():
-	get_tree().change_scene_to_file("res://Main/Scenes/worlds/world2.tscn")
-
+	get_tree().quit()
 
